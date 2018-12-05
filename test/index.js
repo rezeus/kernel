3ec9b694a8be95c1ef168780fd0fd00c292cd084
Ozan Müyesseroğlu <ozanmuyes@gmail.com>
2018-12-05 10:32:07 +0300

    Add project files

---
+++ test/index.js
@@ -0,0 +1,19 @@
+'use strict';
+
+const chai = require('chai');
+
+const AsyncEventEmitter = require('../lib/AsyncEventEmitter');
+
+const expect = chai.expect;
+
+describe('Kernel', function() {
+  describe('AsyncEventEmitter', function() {
+    it('should emit synchronous event', function() {
+      //
+    });
+
+    //
+  });
+
+  // config
+});
